5330ba285f1032ce2abaee5696cfacba1432e7e1
Rashi Smita <rashi.smita2611@gmail.com>
2023-08-17 10:39:49 +0530

    Subscribing to store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-// {
-//     type: 'ADD_MOVIES',
-//     movies: [m1,m2,m3]
-// }
+{
+    type: 'ADD_MOVIES';
+    movies: [m1,m2,m3]
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { legacy_createStore as createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
 
+const store = createStore(movies);
+console.log('store', store); 
+// console.log('BEFORE STATE', store.getState());
+
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{ name: 'WONDER WOMAN'}]
+// });
+// console.log('AFTER STATE', store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ // <React.StrictMode>
+    <App store = {store} />
+ // </React.StrictMode>
 );
 
 
